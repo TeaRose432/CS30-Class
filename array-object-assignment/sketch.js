4317f3b294f3b32dc80ddd890f65494b3c8ebc3e
TeaRose432 <cds4498@hotmail.com>
2025-10-09 09:51:36 -0600

    starting the assigment, downloaded lil guy and hornet images, tried to get them to show up but failed

--- array-object-assignment/sketch.js
+++ array-object-assignment/sketch.js
@@ -5,6 +5,16 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x = 50;
+let y = 50;
+
+let lilGuyImg = {
+
+};
+
+function preload() {
+  lilGuyImg = loadImage("lil-guy.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +22,9 @@ function setup() {
 
 function draw() {
   background(220);
+  showLilGuy;
+}
+
+function showLilGuy() {
+  image(lilGuyImg, x, y, lilGuyImg.width*0.2, lilGuyImg.height*0.2);
 }
