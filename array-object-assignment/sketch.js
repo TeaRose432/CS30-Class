452253f19a49136064b8a156965cb9ef35caa026
TeaRose432 <cds4498@hotmail.com>
2025-10-19 16:19:26 -0600

    got both character choices showing up on the screen

--- array-object-assignment/sketch.js
+++ array-object-assignment/sketch.js
@@ -5,26 +5,38 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x = 50;
-let y = 50;
+let lgx = 100;
+let lgy = 200;
+let hx = 650;
+let hy = 200;
 
 let lilGuyImg = {
+  damage: 10,
+  speed: 3,
+  option: 1,
+};
 
+let hornetImg = {
+  damage: 7,
+  speed: 5,
+  option: 2,
 };
 
 function preload() {
   lilGuyImg = loadImage("lil-guy.png");
+  hornetImg = loadImage("hornet.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(900, 900);
 }
 
 function draw() {
   background(220);
-  showLilGuy;
+  choices();
 }
 
-function showLilGuy() {
-  image(lilGuyImg, x, y, lilGuyImg.width*0.2, lilGuyImg.height*0.2);
+function choices() {
+  image(lilGuyImg, lgx, lgy, lilGuyImg.width*0.2, lilGuyImg.height*0.2);
+  image(hornetImg, hx, hy, hornetImg.width*0.2, hornetImg.height*0.23)
 }
